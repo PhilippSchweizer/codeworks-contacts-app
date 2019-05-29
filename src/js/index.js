aa6c80c6a4ec2c5863392eabc25b10fcb7baefeb
PhilippSchweizer <ph.m.schweizer@outlook.com>
2019-05-29 13:35:52 +0200

    extract input field values into global vars begin contact list logic

--- src/js/index.js
+++ src/js/index.js
@@ -4,16 +4,33 @@ function storeContact() {
 
 
 $( document ).ready(function() {
+  // Globals
+  let firstName = $('#firstName').val();
+  let lastName = $('#lastName').val();
+  let email = $('#email').val();
+  let phone = $('#phone').val();
+  let address = $('#address').val();
+  let contactList = $('#contactList');
+
   // On submit of form, save contact in object 
   $('#saveContact').click(() => {
     let contact = {
-      firstName: $('#firstName').val(),
-      lastName: $('#lastName').val(),
-      email: $('#email').val(),
-      phone: $('#phone').val(),
-      address: $('#address').val(),
+      firstName: firstName,
+      lastName: lastName,
+      email: email,
+      phone: phone,
+      address: address,
     }
+    // Append contact to contact list
+    contactList.html('<li></li>')
     // Save contact with email as identifier in localStorage
     localStorage.setItem($('#email').val(), JSON.stringify(contact));
   })
+
+  // Handle contact list in form of state
+  function setState(newState) {
+    let 
+  }
+
+
 });
