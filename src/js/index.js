98a165ad63c3bcdbeda2638ea408b1d2bfd2b067
PhilippSchweizer <ph.m.schweizer@outlook.com>
2019-05-29 14:52:11 +0200

    Try with state logic

--- src/js/index.js
+++ src/js/index.js
@@ -1,36 +1,28 @@
-function storeContact() {
-  contactsList.push(contact);
-}
-
-
-$( document ).ready(function() {
+$(document).ready(function() {
   // Globals
+  let contactList = $("#contactList");
   let firstName = $('#firstName').val();
   let lastName = $('#lastName').val();
   let email = $('#email').val();
   let phone = $('#phone').val();
   let address = $('#address').val();
-  let contactList = $('#contactList');
 
-  // On submit of form, save contact in object 
-  $('#saveContact').click(() => {
-    let contact = {
-      firstName: firstName,
-      lastName: lastName,
-      email: email,
-      phone: phone,
-      address: address,
-    }
-    // Append contact to contact list
-    contactList.html('<li></li>')
-    // Save contact with email as identifier in localStorage
-    localStorage.setItem($('#email').val(), JSON.stringify(contact));
-  })
-
-  // Handle contact list in form of state
+  let state;
   function setState(newState) {
-    let 
+    for (let name of Object.keys(newState.contacts)) {
+      let contact = $("<li class='list-group-item'><p>'"+ email +"'</p><p>'"+ firstName +"'</p><p>'"+ lastName +"'</p><p>'"+ phone +"'</p><p>'"+ address +"'</p></li>");
+      contactList.append(contact);
+    }
+
+    localStorage.setItem("Contacts", JSON.stringify(newState));
+    state = newState;
   }
-
-
+  
+  $("#saveContact").click(() => {
+    let name = email;
+    if (name)
+      setState({
+        contacts: Object.assign({}, state.contacts, { [name]: "" })
+      });
+  });
 });
