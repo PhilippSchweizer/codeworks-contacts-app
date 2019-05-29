20cbe3ce2f606a977c566dee3b56cb2f06d9e5c3
PhilippSchweizer <ph.m.schweizer@outlook.com>
2019-05-29 12:58:00 +0200

    Write eventHandler functionality for saving new contact

--- src/js/index.js
+++ src/js/index.js
@@ -1,20 +1,19 @@
-let contactsList = [];
-
 function storeContact() {
-  let contact = {
-    firstName: $('#firstName').val(),
-    lastName: $('#lastName').val(),
-    email: $('#email').val(),
-    phone: $('#phone').val(),
-    address: $('#address').val(),
-  }
-  // const email = $('#email');
-  // const phone = $('#phone');
-  // localStorage.setItem('contact', JSON.stringify(contact));
   contactsList.push(contact);
 }
 
 
 $( document ).ready(function() {
-
+  // On submit of form, save contact in object 
+  $('#saveContact').click(() => {
+    let contact = {
+      firstName: $('#firstName').val(),
+      lastName: $('#lastName').val(),
+      email: $('#email').val(),
+      phone: $('#phone').val(),
+      address: $('#address').val(),
+    }
+    // Save contact with email as identifier in localStorage
+    localStorage.setItem($('#email').val(), JSON.stringify(contact));
+  })
 });
