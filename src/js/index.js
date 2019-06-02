7d563badb053036dd20900477a49889be4956a8d
PhilippSchweizer <ph.m.schweizer@outlook.com>
2019-06-02 19:27:47 +0200

    Add contact on submit, close and clear form

--- src/js/index.js
+++ src/js/index.js
@@ -1,21 +1,36 @@
-$( document ).ready(function() {
-  // Globals
-  let firstName = $('#firstName').val();
-  let lastName = $('#lastName').val();
-  let email = $('#email').val();
-  let phone = $('#phone').val();
-  let address = $('#address').val();
-  let contactList = $('#contactList');
-
-  // On submit of form, save contact in object 
-  $('#saveContact').click(() => {
-    console.log('click')
-    let contact = $("<li class='list-group-item'><p>'"+ email +"'</p><p>'"+ firstName +"'</p><p>'"+ lastName +"'</p><p>'"+ phone +"'</p><p>'"+ address +"'</p></li>");
+// IIFE - Immediately Invoked Function Expression
+(function(globalObject) {
+  // The global jQuery object is passed as a parameter
+  globalObject(window.jQuery, window, document);
+})(function($, window, document) {
+  $(function() {
+    // Globals
+    const contactList = $('#contactList');
+    const modal = $('#addContact');
+    const saveContact = $('#saveContact');
     
-    // Append contact to contact list
-    contactList.append(contact);
-    // Save contact with email as identifier in localStorage
-    localStorage.setItem($('#email').val(), JSON.stringify(contact));
-  })
+    // Clear form after submit
+    $('body').on('hidden.bs.modal', function(e) {
+      console.log(e)});
 
+    modal.on('hidden.bs.modal', function() {
+      console.log(this);
+      $(this).find('form')[0].reset();
+    });
+
+    // On submit of form ...
+    saveContact.click((event) => {
+      event.preventDefault();
+      const email = $('#email').val();
+      const firstName = $('#firstName').val();
+      const lastName = $('#lastName').val();
+      const address = $('#address').val();
+      const phone = $('#phone').val();
+      const contact = $(
+        '<li class="list-group-item"><div class="row"><div class="col-sm-3 bg-info rounded">' + email + '</div><div class="col-sm-9"><div class="row"><p class="col">' + firstName + '</p><p class="col">' + lastName + '</p></div><div class="row"><p class="col">' + address + '</p><p class="col">' + phone + '</p></div></div></div></li>');
+
+      contactList.append(contact);
+      modal.modal('hide');
+    });
+  });
 });
